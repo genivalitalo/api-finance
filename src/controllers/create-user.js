5e91472bd7d8687fee3fde7a8967e520dfcd57d3
genivalitalo <italobsilva2108@gmail.com>
2026-04-30 22:57:41 -0300

    feat: created userController and refactory controller

---
+++ src/controllers/create-user.js
@@ -0,0 +1,35 @@
+import { CreateUserCase } from '../use-cases/create-user.js';
+import { badRequest, created, serverError } from './helpers.js';
+import validator from 'validator';
+
+export class CreateUserController {
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      const requireFields = ['first_name', 'last_name', 'email', 'password'];
+
+      for (const field of requireFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing param: ${field}` });
+        }
+      }
+      const isValidPassword = params.password.length < 6;
+      if (isValidPassword) {
+        return badRequest({ message: `Senha menor que 6 caracteres` });
+      }
+      const isEmailValid = validator.isEmail(params.email);
+      if (!isEmailValid) {
+        return badRequest({ message: `E-mail inválido` });
+      }
+
+      const createUserCase = new CreateUserCase();
+      const createdUser = await createUserCase.execute(params);
+
+      return created(createdUser);
+    } catch (error) {
+      console.error(error);
+      return serverError({ message: `Error in server` });
+    }
+  }
+}
--- src/repositories/postgres/create-user.js
+++ src/repositories/postgres/create-user.js
@@ -1,17 +1,21 @@
-import { PostgresHelper } from '../../db/postgres/helper';
+import { PostgresHelper } from '../../db/postgres/helper.js';
 
 export class PostgresCreateUser {
   async execute(createUserParams) {
-    const results = await PostgresHelper.query(
-      'INSERT INTO users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
+    await PostgresHelper.query(
+      'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
       [
-        createUserParams.ID,
+        createUserParams.id,
         createUserParams.first_name,
         createUserParams.last_name,
         createUserParams.email,
         createUserParams.password,
       ],
     );
-    return results[0];
+    const userCreated = await PostgresHelper.query(
+      'SELECT * FROM users WHERE id = $1',
+      [createUserParams.id],
+    );
+    return userCreated[0];
   }
 }
