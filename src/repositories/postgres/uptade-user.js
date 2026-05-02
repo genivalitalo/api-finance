68b372cc4e6e74af1b1277905083d1583cd4c5f2
genivalitalo <italobsilva2108@gmail.com>
2026-05-02 07:10:17 -0300

    feat: add  PostgresUptadeUserRepository

---
+++ src/repositories/postgres/uptade-user.js
@@ -0,0 +1,22 @@
+import { PostgresHelper } from '../../db/postgres/helper.js';
+export class PostgresUptadeUserRepository {
+  async execute(userId, uptadeParams) {
+    const uptadeField = [];
+    const uptadeValues = [];
+    Object.keys(uptadeParams).forEach((key) => {
+      uptadeField.push(`${key} = $${uptadeField.length + 1}`);
+      uptadeValues.push(uptadeParams[key]);
+    });
+    uptadeValues.push(userId);
+
+    const updateQuery = `
+            UPDATE users
+            SET ${uptadeField.join(', ')}
+            WHERE id = ${uptadeValues.length}
+            RETURNING *
+        `;
+
+    const updateUser = await PostgresHelper.query(updateQuery, uptadeValues);
+    return updateUser[0];
+  }
+}
