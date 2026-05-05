7b0f9f01f8afb96e71e9ce20fd459488ea7cb90d
genivalitalo <italobsilva2108@gmail.com>
2026-05-05 00:36:26 -0300

    feat: add PostgresCreateTransactionRepository

---
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -0,0 +1,21 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js';
+
+export class PostgresCreateTransactionRepository {
+  async execute(createTransactionsParams) {
+    const createTransactions = await PostgresHelper.query(
+      `
+        INSERT INTO transactions (id, user_id, name, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING *
+        `,
+      [
+        createTransactionsParams.id,
+        createTransactionsParams.user_id,
+        createTransactionsParams.name,
+        createTransactionsParams.date,
+        createTransactionsParams.amount,
+        createTransactionsParams.type,
+      ],
+    );
+    return createTransactions[0];
+  }
+}
